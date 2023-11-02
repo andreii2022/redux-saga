2178e4595df647265461094b8171899a8c37ec3f
Andrii <nikulin.and90@gmail.com>
2023-11-02 21:26:50 +0200

    add componets

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container pt-3">
+      <div className="row">
+        <div className="col">FORM</div>
+      </div>
+      <div className="row">
+        <div className="col">POSTS</div>
+        <div className="col">FETCED</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FetchedPosts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
