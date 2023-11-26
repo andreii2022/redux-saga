5f1705a78b3e01e730cbd613522de20ee42c44b0
Andrii <nikulin.and90@gmail.com>
2023-11-26 21:16:03 +0200

    Creating an independent thread, handling errors inside the saga.

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,11 +1,12 @@
-import { takeEvery, put, call, fork, all, race } from 'redux-saga/effects';
+import { takeEvery, put, call, fork, all, race, spawn } from 'redux-saga/effects';
 import { GET_NEWS } from '../constans';
 import { setLatestNews, setPopularNews } from '../ections/ectionCreation';
 import { getLatestNews, getPopularNews } from '../../api/index';
 
 export function* handleLatestNews() {
-  const { hits } = yield call(getLatestNews, 'react');
-  yield put(setLatestNews(hits));
+  throw new Error();
+  // const { hits } = yield call(getLatestNews, 'react');
+  // yield put(setLatestNews(hits));
 }
 
 export function* handlePopularNews() {
@@ -14,9 +15,9 @@ export function* handlePopularNews() {
 }
 
 export function* handleNews() {
-  // yield fork(handleLatestNews);
-  // yield fork(handlePopularNews);
-  yield all(call(handleLatestNews), call(handleLatestNews));
+  yield spawn(handleLatestNews);
+  yield spawn(handlePopularNews);
+  // yield all([call(handleLatestNews), call(handlePopularNews)]);
 }
 
 export function* watchClickSaga() {
